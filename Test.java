bcdf2bd73f34099bc2a6893c1d70691bd9684650
Jesse Granger <jesse_granger@fastmail.com>
2020-06-10 10:17:58 -0700

    add the TimeoutTest, and checks against getError()

--- Test.java
+++ Test.java
@@ -25,6 +25,7 @@ public class Test {
 		executor.execute(new UserAgentTest());
 		executor.execute(new InputsTest());
 		executor.execute(new ForceOutcomeTest());
+		executor.execute(new TimeoutTest());
 	}
 
 	public static void _assertEqual(String a, String b) throws AssertionError {
@@ -32,7 +33,7 @@ public class Test {
 		if( a != null && b == null ) assert false : a + " should equal null";
 		if( a == null && b != null ) assert false : "null should equal " + b;
 		// System.out.println("Assert Equal: " + a + " == " + b + " " + a.equals(b));
-		assert a.equals(b) : a + " should equal " + b;
+		assert a.equals(b) : "'" + a + "' should equal '" + b + "'";
 	}
 	public static void _assertOneOf(String a, String... b) {
 		// System.out.println("Assert "+a+" is one of: "+String.join(",", b));
@@ -56,8 +57,10 @@ public class Test {
 			String testName = this.getClass().getName().split("\\$")[1];
 			if( ! finished ) {
 				finished = true;
-				System.out.println(testName + ": " +
-					(optional == null ? "PASSED" : "FAILED " + optional.toString()));
+				System.out.println(
+					(optional == null ? "PASSED" : "FAILED!" + optional.toString())
+					+ " : " + testName
+				);
 			} else {
 				System.out.println(testName + ": Test is faulty: attempted double-finish");
 			}
@@ -79,11 +82,13 @@ public class Test {
 						_assertEqual( firstOutcome.getAgent(), "a-example");
 						_assertOneOf( firstOutcome.getCode(), "A", "B" );
 						_assertEqual( firstOutcome.getPolicy(), "random");
+						assert firstOutcome.getError() == null : "getError() should be null";
 						api.Select( opts, "a-example", new Callback<SelectResponse>() {
 							public void onValue(SelectResponse secondOutcome) {
 								try {
 									_assertEqual( secondOutcome.getCode(), firstOutcome.getCode() );
 									_assertEqual( secondOutcome.getPolicy(), "sticky" );
+									assert secondOutcome.getError() == null : "getError() should be null";
 									finish(null);
 								} catch( AssertionError err ) {
 									finish(err);
@@ -156,6 +161,7 @@ public class Test {
 						_assertEqual( outcome.getAgent(), "a-invalid");
 						_assertEqual( outcome.getCode(), "B" );
 						_assertEqual( outcome.getPolicy(), "none");
+						_assertEqual( outcome.getError().getMessage(), "unknown agent" );
 						finish(null);
 					} catch( AssertionError err ) {
 						finish(err);
@@ -226,6 +232,31 @@ public class Test {
 						_assertEqual( outcome.getAgent(), "a-example");
 						_assertEqual( outcome.getCode(), "D" );
 						_assertEqual( outcome.getPolicy(), "none");
+						_assertEqual( outcome.getError().getMessage(), "forced" );
+						finish(null);
+					} catch( AssertionError err ) {
+						finish(err);
+					}
+				}
+			});
+		}
+	}
+
+	public static class TimeoutTest extends TestCase {
+		@Override public void run() {
+			started = true;
+			RequestOptions opts = new RequestOptions()
+				.setSession("s-" + String.format("%f", Math.random()).replace('.','0'))
+				.setTimeout(1) // after 1 ms, basically instantly
+				.setDefault("a-example", "E");
+			api.Select( opts, "a-example", new Callback<SelectResponse>() {
+				public void onValue(SelectResponse outcome) {
+					try {
+						assert outcome != null : "Outcome cannot be null";
+						_assertEqual( outcome.getAgent(), "a-example");
+						_assertEqual( outcome.getCode(), "E" );
+						_assertEqual( outcome.getPolicy(), "none");
+						_assertEqual( outcome.getError().getMessage(), "Read timed out" );
 						finish(null);
 					} catch( AssertionError err ) {
 						finish(err);
